b47c530296b78e743c34742e539a8ac3997dff48
Marine DELPIERRE <marine.delpierre@student.yncrea.fr>
2021-02-03 10:04:32 +0100

    ajout de la fonction play
    play simule le placement d'un pion (sans condition des déplacements du pion)

--- back/back_GamePlay/classGamePlay.js
+++ back/back_GamePlay/classGamePlay.js
@@ -4,6 +4,7 @@ class Stratego extends Observable{
         this.currentPlayer=0;
         this.grid=new Array(10);
         this.winner=null;
+        this.started= false;
 
         for(let i=0; i<this.grid.length;i++){
             this.grid[i]= new Array(10);
@@ -32,5 +33,20 @@ class Stratego extends Observable{
         return this.grid[i][j];
     }
 
-   
+    play(Pion, i, j){
+    
+        if(this.grid[pos_i][pos_j] == undefined && this.isFinished() == 0 && this.getCaseState() != 'River' && this.started){
+            if(this.currentPlayer == 0){
+                this.grid[i][j] = Pion.name;
+                this.currentPlayer=1;
+            }else{
+                this.grid[i][j] = Pion.name;
+                this.currentPlayer=0;
+            }
+        }
+    }
+
+    isFinished(){
+
+    }
 }
