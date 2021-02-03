4e0ae737527568ed0ce7a7e9d86b981d853395b5
sam5910 <60691551+sam5910@users.noreply.github.com>
2021-02-03 20:45:34 +0100

    2 joueurs
    deux joueurs avec leur nom prenom login et le score

---
+++ back/leaderboard/js/leaderboard.js
@@ -0,0 +1,22 @@
+function leaderboard(){
+
+    let joueur1 = function(){
+        prenom="sam";
+        nom="bel"
+        login= "sam"
+        score="2"
+    }
+    
+    let joueur2 = function(){
+        prenom="pilou"
+        nom="four"
+        login="pilou"
+        score="45"
+    }
+
+   
+
+} 
+
+
+
