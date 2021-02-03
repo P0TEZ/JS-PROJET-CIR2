0193fca7c1aed75400464fb2ceceffff714d9356
sam5910 <60691551+sam5910@users.noreply.github.com>
2021-02-03 09:19:29 +0100

    connexion server
    juste connexion

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express'); 
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http); 
+
+app.use(express.static(__dirname + '/front')); 
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/front/html/page.html');
+}); 
+
+io.on('connection', (socket)=> {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Un eleve s\'est déconnecté');
+    });
+    
+});
+
+http.listen(4200, ()=>{
+    console.log('serveur lance sur le port 4200 http://localhost:4200/ ;');
+});
+
