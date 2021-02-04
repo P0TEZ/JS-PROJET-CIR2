19665dd18353c19d78271558753ffd7144f92c29
Pilou <60597222+PierreLouisF@users.noreply.github.com>
2021-02-04 18:12:47 +0100

    Test Push
    Test Push leaderboard

--- back/leaderboard/js/leaderboard.js
+++ back/leaderboard/js/leaderboard.js
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream
-function leaderboard(){
-
-    let joueurs = [{
-        prenom:"sam",
-        nom:"bel",
-        login: "sam",
-        score:"2",
-=======
 // FICHIER QUI VA RECUP TOUT LES PARTICIPANTS DANS UN TABLEAU 
 
     let joueurs =[{     //exemple de participants  
@@ -15,29 +6,18 @@ function leaderboard(){
         nb_loose: '1',
         score:'2',
         time:'30min',
->>>>>>> Stashed changes
     },
     {
-<<<<<<< Updated upstream
-        prenom:"pilou",
-        nom:"four",
-        login:"pilou",
-        score:"45",
-    }]; 
-
-   
-
-} 
-=======
         username:'pilou',
         nb_win:'6',
         nb_loose: '0',
         score:'20',
         time:'20min',
+    },
+    {
+        username:'marine',
+        nb_win:'0',
+        nb_loose: '0',
+        score:'0',
+        time:'0min',
     }];
-
-
->>>>>>> Stashed changes
-
-
-
