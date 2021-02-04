6eb2e15c4cf8c7b64c4c81d8eb51c5e712bbbe09
sam5910 <60691551+sam5910@users.noreply.github.com>
2021-02-04 17:19:55 +0100

    update de la fonction leaderboard
    tableau avec des participants

--- back/leaderboard/js/leaderboard.js
+++ back/leaderboard/js/leaderboard.js
@@ -1,18 +1,17 @@
 function leaderboard(){
 
-    let joueur1 = function(){
-        prenom="sam";
-        nom="bel"
-        login= "sam"
-        score="2"
-    }
-    
-    let joueur2 = function(){
-        prenom="pilou"
-        nom="four"
-        login="pilou"
-        score="45"
-    }
+    let joueurs = [{
+        prenom:"sam",
+        nom:"bel",
+        login: "sam",
+        score:"2",
+    },
+    {
+        prenom:"pilou",
+        nom:"four",
+        login:"pilou",
+        score:"45",
+    }];
 
    
 
