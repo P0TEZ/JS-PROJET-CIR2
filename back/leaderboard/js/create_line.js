5bab4d3f030096f2ef1bb8de10318dc4f7de743c
sam5910 <60691551+sam5910@users.noreply.github.com>
2021-02-04 10:19:11 +0100

    update tableau jouer, creation du fichier pour creer les lignes
    creation du fichier creat_line

---
+++ back/leaderboard/js/create_line.js
@@ -0,0 +1,5 @@
+//MODULE DANS LEQUEL LES LIGNES VONT ETRE CREEE
+
+let create_line =function (){
+    
+}
---
+++ back/leaderboard/js/leaderboard.js
@@ -0,0 +1,20 @@
+// FICHIER QUI VA RECUP TOUT LES PARTICIPANTS DANS UN TABLEAU 
+
+function leaderboard(){
+
+    let joueurs =[{     //exemple de participants  
+        prenom:"sam",
+        nom:"bel",
+        login: "sam",
+        score:"2",
+    },
+    
+    {
+        prenom="pilou",
+        nom="four",
+        login="pilou",
+        score="45",
+    }];
+
+} 
+
