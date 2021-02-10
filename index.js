39bef5e2221b9a7ddd43fcf0229e56b19178c6f7
Pierre-Louis FOURNET <pierre-louis.fournet@student.yncrea.fr>
2021-02-10 09:08:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const io = require('socket.io')(http);
 app.use(express.static(__dirname + '/back')); 
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/back/html/login.html');
+    res.sendFile(__dirname + '/back/login/html/login.html');
 }); 
 
 io.on('connection', (socket)=> {
