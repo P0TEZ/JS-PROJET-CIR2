da7c3158f0770992dbafb97e18cea9da93cfe328
POTEZ <alexandre.potez@student.yncrea.fr>
2021-02-11 22:24:15 +0100

    gamePlayView systeme de chargement de la grille sur le plateau

--- front/js/gamePlayView.js
+++ front/js/gamePlayView.js
@@ -1,3 +1,5 @@
+let imgLink ="../img/";
+
 class gamePlayView {
     
     constructor(game, name){
@@ -10,9 +12,34 @@ class gamePlayView {
     }
 
     grilleStetter(){
+        let plateau = document.getElementById("plateau");            
         let grille = new Array(10);
-        for (const ligne of grille) {
-            ligne = new Array(10);
+        
+        if(window.innerWidth<window.innerHeight){
+            plateau.style.width="calc(100vw - 10px)";
+            plateau.style.height="calc(100vw - 10px)";
+        }else{
+            plateau.style.width="calc(100vh - 10px)";
+            plateau.style.height="calc(100vh - 10px)";
         }
+
+        for(let row=0;row<grille.length;++row){
+
+            grille[row] = new Array(10);
+
+            for(let column = 0; column<grille[row].length;++column){
+
+                grille[row][column] = document.createElement("img");
+                grille[row][column].className = "pion";
+                grille[row][column].src = " ";
+                grille[row][column].addEventListener("click",event=>{
+                    console.log(row+" / "+column);
+                });
+                plateau.appendChild(grille[row][column]);
+            }
+        }
+        return grille;
     }
 }
+
+let partie = new gamePlayView('test','test');
