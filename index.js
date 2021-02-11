10b106b288e5231bec47c6bba3032127535a8e47
sam5910 <60691551+sam5910@users.noreply.github.com>
2021-02-11 07:41:14 +0100

    changement d'organisation

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-const express = require('express'); 
+const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http); 
+const io = require('socket.io')(http);
+const mysql = require('mysql');
 
-app.use(express.static(__dirname + '/back')); 
+app.use(express.static(__dirname + '/back'));
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/back/login/html/login.html');
-}); 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/front/html/login.html');
+});
 
-io.on('connection', (socket)=> {
+io.on('connection', (socket) => {
     console.log('a user connected');
 
     socket.on('disconnect', () => {
         console.log('Un eleve s\'est déconnecté');
     });
-    
+
 });
 
-http.listen(4200, ()=>{
-    console.log('serveur lance sur le port 4200 http://localhost:4200/ ;');
+http.listen(4222, () => {
+    console.log('serveur lance sur le port 4200 http://localhost:4222/ ;');
 });
+
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'stratego'
+});
+
+db.connect(err => {
+    if (err) throw err;
+    else {
+        console.log("connexion effectue");
+    }
+    let id = '0';
+    let username = "sam";
+    let mdp = "123";
+
+    let data = [id, username, mdp];
+
+    connection.query("INSERT INTO inscrit SET id =?, username=?, mdp=?", data, (err, rslt, field) => {
+        if (err) throw err;
+        console.log(rslt);
+    });
+});
+
 
